2c23bb6c5acca22f8591402263f07fcb77d34992
maqian48 <maqian6@jd.com>
2017-08-02 16:56:34 +0800

    删除冗余注释

--- http/engine.go
+++ http/engine.go
@@ -21,37 +21,6 @@ import (
     /engine/reloadrules,    ReloadRules
 */
 func configEngine() {
-    /*
-	http.HandleFunc("/exit", func(w http.ResponseWriter, r *http.Request) {
-		if g.IsTrustable(r.RemoteAddr) {
-			w.Write([]byte("exiting..."))
-			go func() {
-				time.Sleep(time.Second)
-				os.Exit(0)
-			}()
-		} else {
-			w.Write([]byte("no privilege"))
-		}
-	})
-
-	http.HandleFunc("/config/reload", func(w http.ResponseWriter, r *http.Request) {
-		if g.IsTrustable(r.RemoteAddr) {
-			g.ParseConfig(g.ConfigFile)
-			RenderDataJson(w, g.Config())
-		} else {
-			w.Write([]byte("no privilege"))
-		}
-	})
-
-	http.HandleFunc("/workdir", func(w http.ResponseWriter, r *http.Request) {
-		RenderDataJson(w, file.SelfDir())
-	})
-
-	http.HandleFunc("/ips", func(w http.ResponseWriter, r *http.Request) {
-		RenderDataJson(w, g.TrustableIps())
-	})
-    */
-
 	http.HandleFunc("/engine/version", func(w http.ResponseWriter, r *http.Request) {
 		RenderDataJson(w, funcs.GetVersion())
 	})
