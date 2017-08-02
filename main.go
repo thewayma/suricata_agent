b437ffebb9b651fbdb347e647431a5bc956a835a
maqian48 <maqian6@jd.com>
2017-08-02 17:06:58 +0800

    del version

--- main.go
+++ main.go
@@ -1,10 +1,7 @@
 package main
 
 import (
-    "os"
-    "fmt"
     "flag"
-
     "github.com/thewayma/suricata_agent/g"
     "github.com/thewayma/suricata_agent/cron"
     "github.com/thewayma/suricata_agent/http"
@@ -13,15 +10,8 @@ import (
 
 func main() {
     cfg := flag.String("c", "cfg.json", "configuration file")
-    version := flag.Bool("v", false, "show version")
 
     flag.Parse()
-
-    if *version {
-        fmt.Println(g.VERSION)
-        os.Exit(0)
-    }
-
     g.ParseConfig(*cfg)
 
     g.InitLog()
